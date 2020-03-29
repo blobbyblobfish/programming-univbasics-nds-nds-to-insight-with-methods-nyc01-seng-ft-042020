11d8f294db2a3e5bf1d2aa24f43e48df5ca9bedc
Alice Wang <aywang6@gmail.com>
2020-03-29 17:58:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,22 +5,7 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   
-  gross = 0 
-  row_index = 0 
-  while row_index < director_data.length do 
-    column_index = 0 
-    while column_index < director_data[row_index].length do 
-      inner_len = director_data[row_index][column_index].length
-      inner_index = 0 
-      while inner_index < inner_len do 
-        gross += director_data[row_index][column_index][inner_index][:worldwide_grosses]
-        inner_index += 1 
-      end
-      column_index += 1 
-    end
-    row_index += 1
-end
-end
+
 
 # Write a method that, given an NDS creates a new Hash
 # The return value should be like:
